cdeeb36243f69da7086e519a9fe7d20eec18ee25
Ahmed Hussein <ahmedsabry@outlook.com>
2023-06-23 18:18:11 -0400

    foreach

--- _tdd/script.js
+++ _tdd/script.js
@@ -1,37 +1,102 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
+// function includeHTML() {
+//   var z, i, elmnt, file, xhttp;
+//   /*loop through a collection of all HTML elements:*/
+//   z = document.getElementsByTagName("*");
+//   for (i = 0; i < z.length; i++) {
+//     elmnt = z[i];
+//     /*search for elements with a certain atrribute:*/
+//     file = elmnt.getAttribute("w3-include-html");
+
+//     if (file) {
+//       /*make an HTTP request using the attribute value as the file name:*/
+//       xhttp = new XMLHttpRequest();
+//       xhttp.onreadystatechange = function () {
+//         if (this.readyState == 4) {
+//           if (this.status == 200) {
+//             elmnt.innerHTML = this.responseText;
+//           }
+//           if (this.status == 404) {
+//             elmnt.innerHTML = "Page not found.";
+//           }
+//           /*remove the attribute, and call this function once more:*/
+//           elmnt.removeAttribute("w3-include-html");
+//           includeHTML();
+//         }
+//       };
+//       xhttp.open("GET", file, true);
+//       xhttp.send();
+//       /*exit the function:*/
+//       return;
+//     }
+//   }
+// }
+
+// includeHTML();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const includeHTML = () => {
+  let z, i, elmnt, file, xhttp
+
   /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
+  // els = document.getElementsByTagName("*");
+  const els = document.querySelectorAll('[w3-include-html]')
+  // console.log(els)
+  
+
+  els.forEach(el => {
+    file = el.getAttribute("w3-include-html")
+    // console.log(file)
+    // })
+
+
+  
+    // for (i = 0; i < z.length; i++) {
+    // elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    // file = elmnt.getAttribute("w3-include-html");
 
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
+
         if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
+          this.status == 200 ? el.innerHTML = this.responseText : ''
+          this.status == 404 ? el.innerHTML = "Page not found." : ''
+
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
+          // elmnt.removeAttribute("w3-include-html");
+          el.removeAttribute("w3-include-html");
+          includeHTML()
         }
-      };
+      }
       xhttp.open("GET", file, true);
       xhttp.send();
-      /*exit the function:*/
-      return;
+      
+      return
     }
-  }
+
+    // }
+  });
+
+
 }
 
 includeHTML();
+
+
 
 /* 
 ================================================
@@ -49,7 +114,7 @@ includeHTML();
 // })
 
 const $nav = document.querySelector('#nav')
-console.log($nav)
+// console.log($nav)
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 60) {
     console.log($nav)
