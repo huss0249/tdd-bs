701a9fa3d41da32aaa24208eeef760b141fb1ffc
Ahmed Hussein <ahmedsabry@outlook.com>
2023-06-23 15:20:59 -0400

    copy from tmp

---
+++ tdd/script.js
@@ -0,0 +1,68 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
+
+includeHTML();
+
+/* 
+================================================
+ */
+
+// const $nav = document.querySelector('.navbar')
+// window.addEventListener('scroll', () => {
+// 	if(window.scrollY >= 60) {
+// 		// $nav.classList.add('navbar-scrolled')
+// 		$nav.classList.add('scrolled')
+// 	} else if (window.scrollY <= 60) {
+// 		// $nav.classList.remove('navbar-scrolled')
+// 		$nav.classList.remove('scrolled')
+// 	}
+// })
+
+const $nav = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+	if(window.scrollY >= 60) {
+		// $nav.classList.add('navbar-scrolled')
+		$nav.classList.remove('bg-transparent')
+		$nav.classList.add('bg-dark')
+		$nav.classList.add('transition-3')
+		// $nav.classList.add('scrolled')
+	} else if (window.scrollY <= 60) {
+    // $nav.classList.remove('navbar-scrolled')
+		$nav.classList.remove('bg-dark')
+		// $nav.classList.remove('scrolled')
+		$nav.classList.remove('transition-3')
+		$nav.classList.add('bg-transparent')
+	}
+})
+//--------------------------------------------------------------
